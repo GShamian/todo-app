fc511550f999bc9529de149c36792dade2cd6588
GShamian <gosha.shamian@yandex.ru>
2021-02-25 18:35:19 +0300

    feat: :arrow_up: add CRUD method for items and graceful shutdown

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -6,6 +6,14 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+const (
+	usersTable      = "users"
+	todoListsTable  = "todo_lists"
+	usersListsTable = "users_lists"
+	todoItemsTable  = "todo_items"
+	listsItemsTable = "lists_items"
+)
+
 type Config struct {
 	Host     string
 	Port     string
@@ -15,18 +23,9 @@ type Config struct {
 	SSLMode  string
 }
 
-const (
-	usersTable      = "users"
-	todoListsTable  = "todo_lists"
-	usersListsTable = "users_lists"
-	todoItemsTable  = "todo_items"
-	listsItemTable  = "lists_items"
-)
-
 func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
-
 	if err != nil {
 		return nil, err
 	}
@@ -35,6 +34,6 @@ func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	return db, nil
 
+	return db, nil
 }
